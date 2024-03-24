e27a614f60a9034b21f93e1b8742a9b4ffc714ec
aman-akhtar <amanakhtar881@gmail.com>
2024-03-24 22:36:03 +0530

    slider and gsap feat

--- app.js
+++ app.js
@@ -23,3 +23,84 @@ if (mode == "light") {
   document.documentElement.classList.remove("dark");
   document.documentElement.classList.add("light");
 }
+
+let items = document.querySelectorAll(".slider .list .item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let thumbnail = document.querySelectorAll(".thumbnail .item");
+
+let countItem = items.length;
+let itemActive = 0;
+
+next.onclick = function () {
+  itemActive = itemActive + 1;
+  if (itemActive >= countItem) {
+    itemActive = 0;
+  }
+  showSlider();
+};
+prev.onclick = function () {
+  itemActive = itemActive - 1;
+  if (itemActive < 0) {
+    itemActive = countItem - 1;
+  }
+  showSlider();
+};
+
+//auto run
+let refreshINterval = setInterval(() => {
+  next.click();
+}, 4000);
+function showSlider() {
+  //remove old style of current
+  let itemActiveOld = document.querySelector(".slider .list .item.active");
+  let thumbNailOld = document.querySelector(".thumbnail .item.active");
+  itemActiveOld.classList.remove("active");
+  thumbNailOld.classList.remove("active");
+
+  //active new item
+  items[itemActive].classList.add("active");
+  thumbnail[itemActive].classList.add("active");
+
+  //clear interval
+  clearInterval(refreshINterval);
+  refreshINterval = setInterval(() => {
+    next.click();
+  }, 5000);
+}
+
+//click on thumbnail
+thumbnail.forEach((thumbnail, index) => {
+  thumbnail.addEventListener("click", () => {
+    itemActive = index;
+    showSlider();
+  });
+});
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".info__header", {
+  x: 10,
+  duration: 2,
+});
+gsap.to(".navbar__menu", {
+  y: 10,
+  duration: 2,
+});
+gsap.to(".logo__button", {
+  y: 10,
+  duration: 2,
+});
+gsap.to(".aside__font", {
+  x: -150,
+  y: 170,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".aside__font",
+    scrub: 3,
+    start: "top 0",
+
+    // end: () => `+=${document.querySelector(".square").offsetHeight}`,
+    markers: true,
+    toggleClass: "red",
+  },
+});
